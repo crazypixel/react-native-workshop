59c9e30334e595ce3d0e2b69ab113e970a18f6fd
Shay Keinan <crazypixel@gmail.com>
2017-11-28 17:11:59 +0200

    prep ex1

---
+++ src/views/list.js
@@ -0,0 +1,10 @@
+const DATA = [
+  { key: '0', label: 'Sip a sweating glass of iced tea' },
+  { key: '1', label: 'Have a picnic in the park' },
+  { key: '2', label: 'Eat a slice of watermelon' },
+  { key: '3', label: 'Catch fireflies at night' },
+  { key: '4', label: 'Read a trashy novel' }
+];
+
+const BG_COLOR = '#222222';
+const BTN_COLOR = '#6740b4';
